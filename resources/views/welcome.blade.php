2f05516a6052188aef0a3ca199737de839e2bc3e
caua-dias <caua.dias.cd0@gmail.com>
2025-05-16 18:10:36 -0300

    :lipstick: style: Ajustes na proporção da tela

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel + Vue.js</title>
         @vite(['resources/js/app.js'])
     </head>
-    <body>
-        <div id="app"></div>
+    <body class="h-full">
+        <div class="h-full" id="app"></div>
     </body>
 </html>
