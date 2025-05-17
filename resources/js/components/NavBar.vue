ed2a660c5ca91f8e186dc0d1101d1ef525ed4f30
caua-dias <caua.dias.cd0@gmail.com>
2025-05-17 03:06:15 -0300

    :lipstick: feat: Adição customizada de uma barra de navegação para celular

--- resources/js/components/NavBar.vue
+++ resources/js/components/NavBar.vue
@@ -2,6 +2,11 @@
 
 export default {
     name: 'NavBar',
+    data() {
+        return {
+            isOpen: false
+        };
+    }
 };
 </script>
 
@@ -14,22 +19,56 @@ export default {
                     <img src="../../assets/images/logo-cacto-branco.png" alt="Logo" class="w-10 h-10">
                 </div>
 
-                <div class="flex list-none items-center space-x-40 text-gray-300">
+                <div class="hidden lg:flex list-none items-center space-x-40 text-gray-300">
                     <a href="#" class="hover:underline">Planos</a>
                     <a href="#" class="hover:underline">Simulações</a>
                     <a href="#" class="hover:underline">Produtos</a>
                     <a href="#" class="hover:underline">Recursos</a>
                     <a href="#" class="hover:underline">Nós</a>
-                
+
                 </div>
 
-                <div class="flex items-center space-x-4">
+                <!-- Botão desktop -->
+                <div class="hidden lg:flex items-center space-x-4">
                     <button class=" font-bold hover:bg-gray-400 text-white p-2 rounded-lg" type="submit">Entrar</button>
                     <button class=" bg-slate-50 text-stone-800 p-2 rounded-lg" type="submit">Cadastre-se</button>
                 </div>
 
+                <!-- Botão Mobile -->
+                <div class="lg:hidden">
+                    <button @click="isOpen = true" class="text-white focus:outline-none">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M4 6h16M4 12h16M4 18h16" />
+                        </svg>
+                    </button>
+                </div>
+
+                <!-- Nav Mobile -->
+                
+                <div v-if="isOpen"
+                    class="fixed inset-0 bg-neutral-900 text-gray-100 z-50 flex flex-col items-center justify-center space-y-6 px-4 transition-all duration-300">
+
+                    <button @click="isOpen = false" class="absolute top-6 right-6 text-white text-3xl">
+                        &times;
+                    </button>
+
+                    <a href="#" class="text-2xl font-semibold hover:underline" @click="isOpen = false">Planos</a>
+                    <a href="#" class="text-2xl font-semibold hover:underline" @click="isOpen = false">Simulações</a>
+                    <a href="#" class="text-2xl font-semibold hover:underline" @click="isOpen = false">Produtos</a>
+                    <a href="#" class="text-2xl font-semibold hover:underline" @click="isOpen = false">Recursos</a>
+                    <a href="#" class="text-2xl font-semibold hover:underline" @click="isOpen = false">Nós</a>
+
+                    <div class="pt-8 flex flex-col space-y-4 w-full px-4">
+                        <button class="w-full text-center font-bold hover:bg-gray-700 text-white p-2 rounded-lg"
+                            @click="isOpen = false">Entrar</button>
+                        <button class="w-full text-center bg-slate-50 text-stone-800 p-2 rounded-lg"
+                            @click="isOpen = false">Cadastre-se</button>
+                    </div>
+                </div>
+
             </div>
         </div>
-
     </nav>
 </template>
