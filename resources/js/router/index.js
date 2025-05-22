b89dba7e47b2c5f92a9d2ee3976559b41d0f2119
caua-dias <caua.dias.cd0@gmail.com>
2025-05-22 20:08:19 -0300

    :sparkles: feat: Adição de rotas para as outras páginas

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,9 +1,19 @@
 // resources/js/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
+import Plans from '../pages/Plans.vue'
+import Simulations from '../pages/Simulations.vue'
+import Products from '../pages/Products.vue'
+import Resources from '../pages/Resources.vue'
+import AboutUs from '../pages/AboutUs.vue'
 
 const routes = [
   { path: '/', name: 'home', component: Home },
+  { path: '/plans', name: 'plans', component: Plans },
+  { path: '/simulations', name: 'simulations', component: Simulations },
+  { path: '/products', name: 'products', component: Products },
+  { path: '/resources', name: 'resources', component: Resources },
+  { path: '/aboutus', name: 'aboutus', component: AboutUs },
 ]
 
 const router = createRouter({
