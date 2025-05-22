91e768ab06a226e7f854ee9536b095a6ff6dc541
caua-dias <caua.dias.cd0@gmail.com>
2025-05-22 20:15:48 -0300

    :sparkles: feat: Adição das novas páginas

---
+++ resources/js/pages/Plans.vue
@@ -0,0 +1,17 @@
+<script>
+import NavBar from '../components/NavBar.vue';
+
+export default {
+    name: 'Plans',
+    components: {
+        NavBar,
+    },
+};
+</script>
+
+<template>
+    <NavBar />
+    <div class="h-full w-full flex items-center justify-center">
+      <h1 class="text-4xl font-bold">Página: Planos</h1>
+    </div>
+</template>
---
+++ resources/js/pages/Simulations.vue
@@ -0,0 +1,17 @@
+<script>
+import NavBar from '../components/NavBar.vue';
+
+export default {
+    name: 'Simulations',
+    components: {
+        NavBar,
+    },
+};
+</script>
+
+<template>
+    <NavBar />
+    <div class="h-full w-full flex items-center justify-center">
+      <h1 class="text-4xl font-bold">Página: Sinulações</h1>
+    </div>
+</template>
