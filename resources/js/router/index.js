7c77a6a1ba22dce57dfb69d456ed87560bf8fa63
caua-dias <caua.dias.cd0@gmail.com>
2025-05-27 15:55:13 -0300

    :sparkles: feat: Adição da rota para página de login

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,6 +1,7 @@
 // resources/js/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
+import Login from '../pages/Login.vue'
 import Plans from '../pages/Plans.vue'
 import Simulations from '../pages/Simulations.vue'
 import Products from '../pages/Products.vue'
@@ -9,6 +10,7 @@ import AboutUs from '../pages/AboutUs.vue'
 
 const routes = [
   { path: '/', name: 'home', component: Home },
+  { path: '/login', name: 'login', component: Login },
   { path: '/plans', name: 'plans', component: Plans },
   { path: '/simulations', name: 'simulations', component: Simulations },
   { path: '/products', name: 'products', component: Products },
