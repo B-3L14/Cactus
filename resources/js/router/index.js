54a6febca4b425b9ec535a955c4f1a2240f09c46
caua-dias <caua.dias.cd0@gmail.com>
2025-06-05 17:28:57 -0300

    remove: Remação de rotas (Produtos e Simulações)

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -4,8 +4,6 @@ import Home from '../pages/Home.vue'
 import Login from '../pages/Login.vue'
 import Register from '../pages/Register.vue'
 import Plans from '../pages/Plans.vue'
-import Simulations from '../pages/Simulations.vue'
-import Products from '../pages/Products.vue'
 import Resources from '../pages/Resources.vue'
 import AboutUs from '../pages/AboutUs.vue'
 
@@ -14,8 +12,6 @@ const routes = [
   { path: '/login', name: 'login', component: Login },
   { path: '/register', name: 'register', component: Register },
   { path: '/plans', name: 'plans', component: Plans },
-  { path: '/simulations', name: 'simulations', component: Simulations },
-  { path: '/products', name: 'products', component: Products },
   { path: '/resources', name: 'resources', component: Resources },
   { path: '/aboutus', name: 'aboutus', component: AboutUs },
 ]
